d548c37984622a649e0b508b53a1602304ec7ead
tonyperezjr <perezfilhodev@gmail.com>
2022-05-24 22:37:47 -0300

    git config new account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArrayMethodsComponent } from './pages/array-methods/array-methods.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ArrayMethodsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArrayMethodsComponent } from './pages/array-methods/array-methods.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArrayMethodsComponent
   ],
   imports: [
     BrowserModule,
